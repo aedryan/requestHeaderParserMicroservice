b4fdedbf849ccae5ba91a83760f21e5035d486b7
Andrew Cox <aedryan@gmail.com>
2017-04-13 15:19:20 -0500

    Finished adding functionality.

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ app.set('port', (process.env.PORT || 5000));
 app.use('/favicon.ico', express.static("favicon.ico"));
 
 app.get('/', (req, res) => {
-    res.send("Hello world");
+    const ip = req.ip;
+    const language = req.header("accept-language").replace(/,.+/,'');
+    const operatingSystem = req.header("user-agent").replace(/\).+/,'').slice(req.header("user-agent").indexOf("(") + 1);
+
+    res.json({ip: ip, language: language, OS: operatingSystem});
 });
 
 app.listen(app.get('port'), () => {
